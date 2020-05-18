58f5d1cad2b53b90933845c40e45287efd8097a8
Zuzanna Grobelny <zuzanna.grobelny@gmail.com>
2020-05-18 15:54:18 +0200

    adding pages

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ console.log(name);
 console.log(age);
 console.log(`Witaj, nazywam się ${name} i mam ${age} lat`);
 
-const heading = document.querySelector ('.main-heading--js');
+const heading = document.querySelector ('.main-heading--js')
 heading.innerHTML = `Strona główna Zuzanna`
+const heading = document.querySelector ('main-title--js')
