fd3a720ba9d4f2e494b47e86360ccb145a537262
Zuzanna Grobelny <zuzanna.grobelny@gmail.com>
2020-06-02 17:57:35 +0200

    adding hamburger menu

---
+++ html/secondary.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.querySelector ('.hamburger--js');
+
+hamburger.addEventListener('click',() => {
+const nav = document.querySelector ('.navigation--js');
+nav.classList.toggle ('navigation--open');
+})
